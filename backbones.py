dce9bfda473a87aeb4966b1f1c8ad8d9575226dc
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-06-27 09:55:42 -0400

    Update backbones.py

--- backbones.py
+++ backbones.py
@@ -3,46 +3,7 @@ from tensorflow.keras.layers import Flatten, Dense, BatchNormalization, Softmax,
 from tensorflow.keras.models import Sequential
 
 
-class Backbone(tf.keras.Model):
-    def __init__(self, input_shape, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def call(self, inputs, training=None, mask=None):
-        raise NotImplementedError()
-
-
-class Conv_4_64_test(Backbone):
-    # Encoder as ResNet like CNN with 4 blocks
-    def __init__(self, input_shape, *args, **kwargs):
-        super().__init__(input_shape, *args, **kwargs)
-        self._core = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', input_shape=input_shape),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.ReLU(),
-            tf.keras.layers.MaxPool2D((2, 2)),
-
-            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same'),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.ReLU(),
-            tf.keras.layers.MaxPool2D((2, 2)),
-
-            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same'),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.ReLU(),
-            tf.keras.layers.MaxPool2D((2, 2)),
-
-            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same'),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.ReLU(),
-            tf.keras.layers.MaxPool2D((2, 2)),
-            Flatten()]
-        )
-
-    def call(self, inputs, training=None, mask=None):
-        return self._core.call(inputs, training, mask)
-
-
-def Conv_4_64(input_shape, *args, **kwargs):
+def conv_4_64(input_shape, *args, **kwargs):
     return tf.keras.Sequential([
 
             tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', input_shape=input_shape),
