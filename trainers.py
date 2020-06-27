173ebd27daab14db762e51aa2aa8ef86d79c01dc
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-06-27 09:55:50 -0400

    Update trainers.py

--- trainers.py
+++ trainers.py
@@ -11,13 +11,12 @@ class Trainer:
     def __init__(self,
                  model_manager: NetworkModelManager,
                  dataset: DatasetBase,
+                 network_callback: NetworkManagerCallback = None,
                  network_callback_args=None,
                  **kwargs):
-        if network_callback_args is None:
-            network_callback_args = {
-                "verbose": True,
-                "save_freq": 1
-            }
+
+        # setting phases
+        self.TRAINING_PHASES = [util.TrainingPhase.TRAIN, util.TrainingPhase.VAL]
 
         # setting the model manager
         self.modelManager = model_manager
@@ -32,8 +31,20 @@ class Trainer:
         self.load_on_start = kwargs.get("load_on_start", True)
         self.verbose = kwargs.get("verbose", 1)
 
-        self.network_callback = NetworkManagerCallback(self.modelManager, **network_callback_args)
-        self.current_phase = None
+        # network callback
+        if network_callback_args is None:
+            network_callback_args = {
+                "verbose": False,
+                "save_freq": 1,
+                "early_stopping": True,
+                "patience": 50,
+
+            }
+
+        if network_callback is None:
+            self.network_callback = NetworkManagerCallback(self.modelManager, **network_callback_args)
+        else:
+            self.network_callback = network_callback
 
         # Setting metrics
         # TODO: get the metrics from ModelManager
@@ -50,47 +61,49 @@ class Trainer:
         return self.data_generators
 
     def train(self, epochs=1):
-        self.current_phase = util.TrainingPhase.TRAIN
-
         if self.load_on_start:
             self.modelManager.load()
 
-        # history = self.model.fit(
-        #     self.dataset.get_generator(TrainingPhase.TRAIN, self.modelManager.output_form),
-        #     steps_per_epoch=self.dataset.train_length // self.dataset.batch_size,
-        #     epochs=epochs,
-        #     validation_data=self.dataset.get_generator(TrainingPhase.VAL, self.modelManager.output_form),
-        #     validation_steps=self.dataset.val_length // self.dataset.batch_size,
-        #     callbacks=[self.network_callback] if self.use_saving_callback else [],
-        #     verbose=self.verbose,
-        #     initial_epoch=self.modelManager.current_epoch,
-        # )
-
         history = {
-            _metric: []
-            for _metric in ["loss", "accuracy"]  # TODO: get metrics from modelManager
+            _p.value: {
+                _metric: []
+                for _metric in self.running_metrics.keys()  # TODO: get metrics from modelManager
+            }
+            for _p in self.TRAINING_PHASES
         }
 
+        epochs_range = range(self.modelManager.current_epoch, epochs)
+
         self.progress = tqdm(
-            iterable=range(self.modelManager.current_epoch, epochs),
+            iterable=epochs_range,
             unit="epoch",
         )
-        self.progress.set_description_str(str(self.current_phase.value))
-        for _ in range(len(self.progress)):
-            epoch = self.progress.n
-
-            logs = self.do_epoch(epoch, self.current_phase)
+        for epoch in epochs_range:
+            logs = self.do_epoch(epoch)
 
             # update history
-            for _metric in history:
-                history[_metric].append(logs.get(_metric))
+            for _p in self.TRAINING_PHASES:
+                for _metric in history[_p.value]:
+                    history[_p.value][_metric].append(logs[_p.value].get(_metric))
 
             # update progress
-            self.progress.set_postfix_str(f"train_loss: {logs.get('loss')} - train_acc: {logs.get('accuracy')}")
+            self.progress.set_postfix_str(
+                ' - '.join([
+                    f"{_p.value}_{_metric_name}: {_metric}"
+                    for _p in self.TRAINING_PHASES for _metric_name, _metric in logs[_p.value].items()
+                    ])
+            )
             # self.progress.update()
+
+            # EarlyStopping
+            if self.network_callback.early_stopping_triggered:
+                break
 
         self.progress.close()
         self.modelManager.save_weights()
+
+        self.test()
+
         return history
 
     def loss(self, x, y, training):
@@ -104,7 +117,21 @@ class Trainer:
             loss_value = self.loss(inputs, targets, training=True)
         return loss_value, tape.gradient(loss_value, self.model.trainable_variables)
 
-    def do_epoch(self, epoch: int, phase: util.TrainingPhase):
+    def do_epoch(self, epoch: int):
+        # calling the callback
+        self.network_callback.on_epoch_begin(epoch)
+
+        logs = {
+            _p.value: self.do_phase(epoch, _p)
+            for _p in self.TRAINING_PHASES
+        }
+
+        # calling the callback
+        self.network_callback.on_epoch_end(epoch, logs=logs)
+
+        return logs
+
+    def do_phase(self, epoch: int, phase: util.TrainingPhase):
         epoch_loss_avg = tf.keras.metrics.Mean()
         epoch_accuracy = tf.keras.metrics.Accuracy()
         metrics = dict(zip(self.model.metrics_names, self.model.metrics))
@@ -144,13 +171,23 @@ class Trainer:
 
         return epoch_logs
 
+    def test(self):
+        pass
+
 
 class FewShotTrainer(Trainer):
     def __init__(self, model_manager: NetworkModelManager, dataset: DatasetBase, **kwargs):
         self.n_way = kwargs.get("n_way", 20)
         self.n_shot = kwargs.get("n_shot", 5)
         self.n_query = kwargs.get("n_query", 1)
-        self.n_episodes = kwargs.get("n_episodes", 10)
+        self.n_train_episodes = kwargs.get("n_train_episodes", 10)
+        self.n_val_episodes = kwargs.get("n_val_episodes", 10)
+        self.n_test_episodes = kwargs.get("n_test_episodes", 600)
+
+        self.n_training_episodes = {
+            util.TrainingPhase.TRAIN: self.n_train_episodes,
+            util.TrainingPhase.VAL: self.n_val_episodes,
+        }
 
         super().__init__(model_manager, dataset, **kwargs)
 
@@ -166,24 +203,30 @@ class FewShotTrainer(Trainer):
         }
         return self.data_generators
 
-    def do_epoch(self, epoch: int, phase: util.TrainingPhase):
+    def do_phase(self, epoch: int, phase: util.TrainingPhase):
+        self.progress.set_description_str(str(phase.value))
+
         # reset the metrics
         for _metric in self.running_metrics:
             self.running_metrics[_metric].reset_states()
 
-        # calling the callback
-        self.network_callback.on_epoch_begin(epoch)
+        total_episodes = sum(list(self.n_training_episodes.values()))
 
         _data_itr = iter(self.data_generators[phase])
-        for episode_idx in range(self.n_episodes):
+        for episode_idx in range(self.n_training_episodes[phase]):
             support, query = next(_data_itr)
 
             # Optimize the model
             # Forward & update gradients
-            with tf.GradientTape() as tape:
-                loss, acc = self.model.call(support, query)  # TODO: ask ModelManeger to get metrics dict as logs
-            grads = tape.gradient(loss, self.model.trainable_variables)
-            self.model.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+            if phase == util.TrainingPhase.TRAIN:
+                with tf.GradientTape() as tape:
+                    loss, acc = self.model.call(support, query)  # TODO: ask ModelManager to get metrics dict as logs
+                grads = tape.gradient(loss, self.model.trainable_variables)
+                self.model.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+            elif phase == util.TrainingPhase.VAL:
+                loss, acc = self.model.call(support, query)
+            else:
+                raise NotImplementedError(f"Training phase: {phase} not implemented")
 
             # Track progress
             # TODO: get metrics automatically
@@ -191,17 +234,59 @@ class FewShotTrainer(Trainer):
             self.running_metrics["accuracy"].update_state(acc)
 
             # update progress
-            self.progress.update(1 / self.n_episodes)
-            self.progress.set_postfix_str(f"episode: {episode_idx}/{self.n_episodes} -> "
-                                          + ' - '.join([f"{k}: {v.result():.3f}"
+            self.progress.update(1 / total_episodes)
+            self.progress.set_postfix_str(f"episode: {episode_idx}/{self.n_training_episodes[phase]} -> "
+                                          + ' - '.join([f"{phase.value}_{k}: {v.result():.3f}"
                                                        for k, v in self.running_metrics.items()]))
 
-        epoch_logs = {k: v.result() for k, v in self.running_metrics.items()}
+        phase_logs = {k: v.result().numpy() for k, v in self.running_metrics.items()}
 
-        # calling the callback
-        self.network_callback.on_epoch_end(epoch, logs=epoch_logs)
+        return phase_logs
 
-        return epoch_logs
+    def test(self):
+        phase = util.TrainingPhase.TEST
+
+        self.progress = tqdm(
+            iterable=range(self.n_test_episodes),
+            unit="episode",
+        )
+        self.progress.set_description_str("Test")
+
+        # reset the metrics
+        for _metric in self.running_metrics:
+            self.running_metrics[_metric].reset_states()
+
+        _data_itr = iter(self.data_generators[phase])
+        for episode_idx in range(self.n_test_episodes):
+            support, query = next(_data_itr)
+
+            # Optimize the model
+            # Forward & update gradients
+            loss, acc = self.model.call(support, query)
+
+            # Track progress
+            # TODO: get metrics automatically
+            self.running_metrics["loss"].update_state(loss)
+            self.running_metrics["accuracy"].update_state(acc)
+
+            # update progress
+            self.progress.update(1)
+            self.progress.set_postfix_str(' - '.join([f"{phase.value}_{k}: {v.result():.3f}"
+                                                     for k, v in self.running_metrics.items()]))
+
+        self.progress.close()
+        phase_logs = {k: v.result().numpy() for k, v in self.running_metrics.items()}
+
+        if self.verbose:
+            print("\n--- Test results --- \n"
+                  f"model: {self.modelManager.name} \n"
+                  f"Few Shot params: {self.n_way}-way {self.n_shot}-shot \n"
+                  f"Train episodes: {self.n_train_episodes * (self.modelManager.current_epoch + 1)} \n"
+                  f"Test episodes: {self.n_test_episodes} \n"
+                  f"Mean accuracy: {phase_logs.get('accuracy')*100:.2f}% \n"
+                  f"{'-'*35}")
+
+        return phase_logs
 
 
 if __name__ == '__main__':
