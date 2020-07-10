c3c32af23abde80abe868f123c5928dd53313507
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-07-10 10:50:14 -0400

    Update models.py

--- modules/models.py
+++ modules/models.py
@@ -136,7 +136,7 @@ class Prototypical(tf.keras.Model):
         log_p_y = tf.nn.log_softmax(-dists, axis=-1)
         log_p_y = tf.reshape(log_p_y, [self.n_class, n_query, -1])
 
-        loss_few = -tf.reduce_mean(tf.reshape(tf.reduce_sum(tf.multiply(y_onehot, log_p_y), axis=-1), [-1]))
+        loss_few = -tf.reduce_mean(tf.reshape(tf.reduce_sum(tf.multiply(y_onehot, tf.cast(log_p_y, tf.float32)), axis=-1), [-1]))
         eq = tf.cast(
             tf.equal(
                 tf.cast(tf.argmax(log_p_y, axis=-1), tf.int32),
