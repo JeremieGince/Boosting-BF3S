2f63251e20f1fb74f256b4829256d29bcc1d4969
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-07-18 09:27:04 -0400

    Update prototypical_rotation_config.py

--- config/prototypical_rotation_config.py
+++ config/prototypical_rotation_config.py
@@ -22,14 +22,14 @@ config = {
     "Model_parameters": {
         "name": f"prototypical_rot-{backbone}_"
                 f"{way}way{shot}shot_{t_way}tway{t_shot}tshot_"
-                f"alpha100e-2_SGD1e-3",
+                f"alpha100e-2_SGD1e-2",
         "method": FewShotImgLearner.Method.PrototypicalNet,
         "alpha": 1.00,
         "sl_boosted_type": util.SLBoostedType.ROT,
         "sl_kwargs": {
             "hidden_neurons": [640 for _ in range(4)]
         },
-        "learning_rate": 1e-3,
+        "learning_rate": 1e-2,
         "optimizer_args": {
             "momentum": 0.9,
             "decay": 5e-4,
