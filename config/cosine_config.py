c427b8592ee977a76d73760f88354ae26a4b6324
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-08-07 12:36:37 -0400

    Update cosine_config.py

--- config/cosine_config.py
+++ config/cosine_config.py
@@ -7,7 +7,7 @@ shot = 5
 t_shot = 5
 backbone = "conv-4-64"
 
-batch_epochs = 00
+batch_epochs = 10
 
 config = {
     "Tensorflow_constants": {
@@ -21,8 +21,9 @@ config = {
     "Model_parameters": {
         "name": f"cosine_classifier-{backbone}_"
                 f"{way}way{shot}shot_{t_way}tway{t_shot}tshot"
-                f"_15",
+                f"_17",
         "method": FewShotImgLearner.Method.CosineNet,
+        "backbone": backbone,
         "alpha": None,
         "sl_kwargs": None,
         "n_cls_base": 64,
@@ -48,14 +49,14 @@ config = {
 
         # optimizer
         "learning_rate": 1e-3,
-        # "optimizer_args": {
-        #     "momentum": 0.9,
-        #     "decay": 5e-4,
-        #     "nesterov": True,
-        # },
-        # "optimizer": tf.keras.optimizers.SGD,
+        "optimizer_args": {
+            "momentum": 0.9,
+            "decay": 5e-4,
+            "nesterov": True,
+        },
+        "optimizer": tf.keras.optimizers.SGD,
         # "optimizer_args": {},
-        "optimizer": tf.keras.optimizers.Adam,
+        # "optimizer": tf.keras.optimizers.Adam,
     },
 
     "FewShot_Trainer_parameters": {
