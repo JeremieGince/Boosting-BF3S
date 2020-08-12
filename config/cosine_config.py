26f1041fe066607e43743e56af3195d4e1dce426
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-08-12 14:24:58 -0400

    Update cosine_config.py

--- config/cosine_config.py
+++ config/cosine_config.py
@@ -7,7 +7,7 @@ shot = 5
 t_shot = 5
 backbone = "conv-4-64"
 
-batch_epochs = 30
+batch_epochs = 100
 
 config = {
     "Tensorflow_constants": {
@@ -21,7 +21,7 @@ config = {
     "Model_parameters": {
         "name": f"cosine_classifier-{backbone}_"
                 f"{way}way{shot}shot_{t_way}tway{t_shot}tshot"
-                f"_19",
+                f"_20",
         "method": FewShotImgLearner.Method.CosineNet,
         "backbone": backbone,
         "alpha": None,
