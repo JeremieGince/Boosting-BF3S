4786559c2dd6e4de2e990c6b0924c4a6230ca88d
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-08-12 22:10:20 -0400

    Update backbones.py

--- modules/backbones.py
+++ modules/backbones.py
@@ -30,3 +30,30 @@ def conv_4_64(input_shape, *args, **kwargs):
             name="Conv-4-64"
         )
 
+
+def conv_4_64_avg_pool(input_shape, *args, **kwargs):
+    return tf.keras.Sequential([
+
+            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', input_shape=input_shape),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.MaxPool2D((2, 2)),
+
+            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.MaxPool2D((2, 2)),
+
+            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.MaxPool2D((2, 2)),
+
+            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.GlobalAveragePooling2D(),
+
+            Flatten(dtype=tf.float32)],
+            name="Conv-4-64"
+        )
