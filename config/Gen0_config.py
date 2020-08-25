503e61f4bbf8cfdae602ae04b3608aeb6e26ec67
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-08-24 20:39:31 -0400

    Update Gen0_config.py

--- config/Gen0_config.py
+++ config/Gen0_config.py
@@ -47,14 +47,14 @@ config = {
 
         # optimizer
         "learning_rate": 1e-3,
-        "optimizer_args": {
-            "momentum": 0.9,
-            "decay": 5e-4,
-            "nesterov": True,
-        },
-        "optimizer": tf.keras.optimizers.SGD,
-        # "optimizer_args": {},
-        # "optimizer": tf.keras.optimizers.Adam,
+        # "optimizer_args": {
+        #     "momentum": 0.9,
+        #     "decay": 5e-4,
+        #     "nesterov": True,
+        # },
+        # "optimizer": tf.keras.optimizers.SGD,
+        "optimizer_args": {},
+        "optimizer": tf.keras.optimizers.Adam,
     },
 
     "FewShot_Trainer_parameters": {
