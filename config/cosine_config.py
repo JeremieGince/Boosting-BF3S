789225f082387245ac2092ecd5e1f5410997e912
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-08-26 16:20:53 -0400

    Update cosine_config.py

--- config/cosine_config.py
+++ config/cosine_config.py
@@ -67,8 +67,14 @@ config = {
 
             # optimizer
             "learning_rate": 1e-3,
-            "optimizer_args": {},
-            "optimizer": tf.keras.optimizers.Adam,
+            "optimizer_args": {
+                "momentum": 0.9,
+                "decay": 5e-4,
+                "nesterov": True,
+            },
+            "optimizer": tf.keras.optimizers.SGD,
+            # "optimizer_args": {},
+            # "optimizer": tf.keras.optimizers.Adam,
         },
         {
             "trainer_type": TrainerType.EpisodicTrainer,
