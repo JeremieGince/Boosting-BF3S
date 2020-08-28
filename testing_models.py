666e91088fb30a693ceaf5613a922a6b474e4ddb
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-08-28 17:06:25 -0400

    outputfiles

--- testing_models.py
+++ testing_models.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     else:
         data_dir = r"D:\Datasets\mini-imagenet"
 
-    outfiles_path = "training_data/training_outputfiles"
+    outfiles_path = "training_data/testing_outputfiles"
     os.makedirs(outfiles_path, exist_ok=True)
 
     mth_list = ["proto", "proto_rot", "cosine", "cosine_rot", "Gen0", "Gen1"]
     for mth in mth_list:
         with open(f'{outfiles_path}/{mth}_out.txt', 'w') as f:
-            process = subprocess.call(['python', 'training_model.py', data_dir, mth], stdout=f)
+            process = subprocess.call(['python', 'testing_model.py', data_dir, mth], stdout=f)
             print(process)
