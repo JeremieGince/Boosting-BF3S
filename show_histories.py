baecf1fae0fa5a082d335ab1649f6ad1b55c53bc
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-08-31 20:09:48 -0400

    Update show_histories.py

--- show_histories.py
+++ show_histories.py
@@ -1,13 +1,21 @@
 import json, os
 from modules.util import plotHistory
 
-filename: str = "training_data/prototypical_few_shot_learner-conv-4-64_backbone_30way5shot_5tway5tshot_085_c_67acc/cp-history.json"
+filename: str = "training_data/Gen0-conv-4-64_5tway5tshot_97/cp-history.json"
 model_name = os.path.basename(os.path.dirname(filename))
+
+try:
+    with open(os.path.dirname(filename)+"/test_results.json") as test_file:
+        print(json.load(test_file).get("pprint"))
+except FileNotFoundError:
+    print(f"No test has been executed")
 
 with open(filename, 'r') as file:
     plotHistory(
         json.load(file),
-        savefig=False,
+        savefig=True,
         savename=f"training_curve_{model_name}"
     )
+
+
 
